e237e3128d2f1b714c1855b4bed5cc2ffb32d3cb
PanchoClaria <>
2024-06-16 18:51:57 -0300

    Add try/catch and null checks to prevent null pointer exceptions being thrown

--- src/android/CameraPreview.java
+++ src/android/CameraPreview.java
@@ -345,11 +345,15 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
           containerView.bringToFront();
         }
 
-        //add the fragment to the container
-        FragmentManager fragmentManager = cordova.getActivity().getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(containerView.getId(), fragment);
-        fragmentTransaction.commit();
+        try {
+          // add the fragment to the container
+          FragmentManager fragmentManager = cordova.getActivity().getFragmentManager();
+          FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+          fragmentTransaction.add(containerView.getId(), fragment);
+          fragmentTransaction.commit();
+        } catch (Exception e) {
+          // prevent Can not perform this action after onSaveInstanceState
+        }
       }
     });
 
@@ -371,7 +375,9 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 
     takeSnapshotCallbackContext = callbackContext;
 
-    fragment.takeSnapshot(quality);
+    if (fragment != null) {
+      fragment.takeSnapshot(quality);
+    }
     return true;
   }
 
@@ -970,7 +976,7 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 
     List<String> supportedFlashModes;
     supportedFlashModes = camera.getParameters().getSupportedFlashModes();
-    if (supportedFlashModes.indexOf(flashMode) > -1) {
+    if (supportedFlashModes != null && supportedFlashModes.indexOf(flashMode) > -1) {
       params.setFlashMode(flashMode);
     } else {
       callbackContext.error("Flash mode not recognised: " + flashMode);
@@ -998,12 +1004,16 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
       return true;
     }
 
-    FragmentManager fragmentManager = cordova.getActivity().getFragmentManager();
-    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    fragmentTransaction.remove(fragment);
-    fragmentTransaction.commit();
-    fragment = null;
-
+    try {
+      FragmentManager fragmentManager = cordova.getActivity().getFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+      fragmentTransaction.remove(fragment);
+      fragmentTransaction.commit();
+      fragment = null;
+    } catch (Exception e) {
+      // prevent null pointer exception "Cant perform this action after
+      // onSaveInstanceState"
+    }
     callbackContext.success();
     return true;
   }
